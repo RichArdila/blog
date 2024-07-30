22cf6cb64eba9e4750363c9ee451d1f8b2d62e88
Richard <richardardilaz17@hotmail.com>
2024-07-30 19:28:40 -0400

    Created file AuthOptions in lib

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,6 @@
-import NextAuth, { SessionOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/lib/prisma";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        if (!credentials) {
-          return null;
-        }
+const handler = NextAuth(authOptions);
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.username },
-        });
-        console.log("User fetched:", user);
-        if (user && user.password === credentials.password) {
-          return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            role: user.role,
-          };
-        }
-        return null;
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
-export const handle = NextAuth(authOptions);
-export { handle as GET, handle as POST };
+export { handler as GET, handler as POST };
---
+++ lib/authOptions.ts
@@ -0,0 +1,34 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.username },
+        });
+        console.log("User fetched:", user);
+        if (user && user.password === credentials.password) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          };
+        }
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
